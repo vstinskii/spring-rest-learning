032a230861add2012aaef2c4e9ec315f8f07501b
Vitalij Stinskij <vstinskij@gmail.com>
2021-06-11 14:25:02 +0300

    refactor loading starships data

--- src/main/java/com/company/control/StarshipRestController.java
+++ src/main/java/com/company/control/StarshipRestController.java
@@ -3,6 +3,7 @@ package com.company.control;
 import com.company.model.Starship;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,29 +11,34 @@ import java.util.List;
 @RequestMapping("/")
 public class StarshipRestController {
 
+    private List<Starship> starships;
+
+    @PostConstruct
+    public void loadStarshipsData() {
+        starships = new ArrayList<>();
+
+        starships.add(new Starship(1,"A441",15,0,true));
+        starships.add(new Starship(2,"A442",20,5,true));
+        starships.add(new Starship(3,"A445",15,10,true));
+
+    }
+
     @GetMapping("/ship")
     public String getModelShip() {
         return "A441";
     }
 
+
+
+
     @GetMapping("/ships")
     public List<Starship> getStarships() {
-        List<Starship> starships = new ArrayList<>();
-
-        starships.add(new Starship(1,"A441",15,0,true));
-        starships.add(new Starship(2,"A442",20,5,true));
-        starships.add(new Starship(3,"A445",15,10,true));
 
         return starships;
     }
 
     @RequestMapping("/ships/get-ship-by-id/{starshipId}")
     public Starship getStarshipById(@PathVariable int starshipId) {
-        List<Starship> starships = new ArrayList<>();
-
-        starships.add(new Starship(1,"A441",15,0,true));
-        starships.add(new Starship(2,"A442",20,5,true));
-        starships.add(new Starship(3,"A445",15,10,true));
 
         return starships.get(starshipId);
     }
