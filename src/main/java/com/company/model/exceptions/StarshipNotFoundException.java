2179c52a1d1f37d89c5406109ab04beb55834878
Vitalij Stinskij <vstinskij@gmail.com>
2021-06-26 15:20:30 +0300

    added two super constructors for StarshipNotFoundException class

--- src/main/java/com/company/model/exceptions/StarshipNotFoundException.java
+++ src/main/java/com/company/model/exceptions/StarshipNotFoundException.java
@@ -2,7 +2,15 @@ package com.company.model.exceptions;
 
 public class StarshipNotFoundException extends RuntimeException {
 
+    public StarshipNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
     public StarshipNotFoundException(String message) {
         super(message);
+    }
+
+    public StarshipNotFoundException(Throwable cause) {
+        super(cause);
     }
 }
